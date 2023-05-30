1b004147671e1f53b3d751a06c3f50825aa98102
kkyuwoo <ggya5@naver.com>
2023-05-30 11:46:59 +0900

    feat: 윤년 알아맞추기 TDD

--- src/test/java/day1/LeapyearTest.java
+++ src/test/java/day1/LeapyearTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LeapyearTest {
-
     @Test
     void checkLeapYear() {
         Assertions.assertEquals(false, Leapyear.checkLeapYear(1700));
+    }
+    @Test
+    void testLearYear() {
+        Assertions.assertEquals(true, Leapyear.isLeapYear(2000));
+        Assertions.assertEquals(false, Leapyear.isLeapYear(1900));
+        Assertions.assertEquals(true, Leapyear.isLeapYear(2400));
     }
 }
