6e6863c30dec175b267dec63895884739aca1eab
kkyuwoo <ggya5@naver.com>
2023-05-31 14:21:13 +0900

    feat: Person 전체 조회

--- src/main/java/day2/DBMain.java
+++ src/main/java/day2/DBMain.java
@@ -1,15 +1,40 @@
 package day2;
 
 import day2.model.BaseDAO;
+import day2.model.Person;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
         dbMain.initPerson();
-        dbMain.insertPerson("leo");
-        dbMain.insertPerson("yui");
+        System.out.println( dbMain.insertPerson("leo") );
+        System.out.println( dbMain.insertPerson("yui") );
+        List<Person> personList = dbMain.findAllPerson();
+        System.out.println(personList.toString());
+    }
+
+    private List<Person> findAllPerson() {
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result.add(new Person(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return result;
     }
 
     private int insertPerson(String name) {
---
+++ src/main/java/day2/model/Person.java
@@ -0,0 +1,24 @@
+package day2.model;
+
+public class Person {
+    int id;
+    String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,03d %s", id, name);
+    }
+}
