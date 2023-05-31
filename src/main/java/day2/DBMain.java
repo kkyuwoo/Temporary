3801023d41b8c01663da98dc920f5f6e046a264e
kkyuwoo <ggya5@naver.com>
2023-05-31 13:05:03 +0900

    feat:Person table 초기화(생성과 기본값 입력)

--- src/main/java/day2/DBMain.java
+++ src/main/java/day2/DBMain.java
@@ -4,7 +4,52 @@ import java.sql.*;
 
 public class DBMain {
     public static void main(String[] args) {
-        getCodeName();
+        initPerson();
+    }
+
+    private static void initPerson() {
+        Connection conn = null;
+        Statement smt = null;
+        PreparedStatement psmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
+            smt = conn.createStatement();
+            smt.setQueryTimeout(30);
+
+            smt.executeUpdate("drop table if exists person");
+            smt.executeUpdate("create table person (id integer, name string)");
+            smt.executeUpdate("insert into person values(1, 'leo')");
+            smt.executeUpdate("insert into person values(2, 'yui')");
+            rs = smt.executeQuery("select * from person");
+            while(rs.next()) {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (smt != null) smt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (psmt != null) psmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void getCodeName() {
