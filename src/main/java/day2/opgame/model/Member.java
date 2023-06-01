de78de8f23046d09558781f5d9b71141615ceae6
kkyuwoo <ggya5@naver.com>
2023-06-01 11:28:09 +0900

    feat: Member DAO, DTO 구현

--- src/main/java/day2/opgame/model/Member.java
+++ src/main/java/day2/opgame/model/Member.java
@@ -1,12 +1,19 @@
 package day2.opgame.model;
 
 public class Member {
+    String id;
+    boolean isLogined;
+    public Member(String id, boolean isLogined) {
+        this.id = id;
+        this.isLogined = isLogined;
+    }
+
     public String getID() {
-        return null;
+        return id;
     }
 
     public boolean isLogined() {
-        return false;
+        return isLogined;
     }
 
 }
--- src/main/java/day2/opgame/model/MemberDAO.java
+++ src/main/java/day2/opgame/model/MemberDAO.java
@@ -1,20 +1,102 @@
 package day2.opgame.model;
 
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class MemberDAO extends BaseDAO {
     public void initMember() {
+        String sql = """
+                create table opg_member (
+                seq integer primary key autoincrement,
+                id text NOT NULL,
+                pw text NOT NULL,
+                islogined integer default '0',
+                created_at text default current_timestamp,
+                updated_at text default current_timestamp
+                )
+                """;
+        try {
+            getConn();
+            smt = conn.createStatement();
+
+            smt.executeUpdate("drop table if exists opg_member");
+            smt.executeUpdate(sql);
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            close();
+        }
     }
 
     public Optional<Member> login(String id, String pw) {
+        String sql = """
+                update opg_member set islogined='1' where id=? and pw=?
+                """;
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+            psmt.setString(2, pw);
+            int cnt = psmt.executeUpdate();
+            if (cnt > 0) {
+                psmt = conn.prepareStatement("select id, islogined from opg_member where id=?");
+                psmt.setString(1, id);
+                psmt.executeQuery();
+                if (rs.next()) {
+                    String rid = rs.getString(1);
+                    boolean risLogined = rs.getBoolean(2);
+                    return Optional.of(new Member(rid, risLogined));
+                }
+            }
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            close();
+        }
         return Optional.empty();
     }
 
-    public void join(String id, String pw) {
-
+    public int join(String id, String pw) {
+        int cnt = 0;
+        String sql = "insert into person(id, pw) values(?, ?)";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+            psmt.setString(2, pw);
+            cnt = psmt.executeUpdate();
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            close();
+        }
+        return cnt;
     }
 
     public Optional<Member> logout(String id) {
-        return null;
+        String sql = """
+                update opg_member set islogined='0' where id=?
+                """;
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            psmt.setString(1, id);
+            int cnt = psmt.executeUpdate();
+            if (cnt > 0) {
+                psmt = conn.prepareStatement("select id, islogined from opg_member where id=?");
+                psmt.setString(1, id);
+                psmt.executeQuery();
+                if (rs.next()) {
+                    String rid = rs.getString(1);
+                    boolean risLogined = rs.getBoolean(2);
+                    return Optional.of(new Member(rid, risLogined));
+                }
+            }
+        } catch(SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            close();
+        }
+        return Optional.empty();
     }
 }
